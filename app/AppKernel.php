d909ac1e764e0bd116937673fe070998d30a792b
fageerman <f.a.geerman@gmail.com>
2015-11-10 14:53:59 -0400

    Doctrine migration installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new JordiLlonch\Bundle\CrudGeneratorBundle\JordiLlonchCrudGeneratorBundle(),
             new Serlimar\SerlEdgeBundle\SerlimarSerlEdgeBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
